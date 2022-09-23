8d5e87b723094fa4cc233c6d4ed83b6691651731
JMPMorgan <jonathan.morganp@gmail.com>
2022-09-22 22:20:51 -0500

    Add jwt helper and middleware

---
+++ helpers/generateJWT.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (uid = "") => {
+  return new Promise((resolve, reject) => {
+    const payload = { uid };
+    jwt.sign(payload, process.env.SPK, {
+      expiresIn: "8h",
+    }),
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          reject("No JWT");
+        } else {
+          resolve(token);
+        }
+      };
+  });
+};
+
+module.exports = {
+  generateJWT,
+};
---
+++ middlewares/validateJWT.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const validateJWT = async (req, res, next) => {
+  const token = req.header("token");
+  if (!token) {
+    return res.status(401).json({
+      msg: "Token dont exits",
+    });
+  }
+  try {
+    const { id } = jwt.verify(token, process.env.SPK);
+    req.id = id;
+    const userAuth = await User.findById(uid);
+    if (!userAuth.status) {
+      return res.status(401).json({
+        msg: "User Dont Exits",
+      });
+    }
+    req.userAuth = userAuth;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      msg: "Invalid Token",
+    });
+  }
+};
